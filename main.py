62975a41437fc57306fbf4b00263f9e26bc1e24f
天仙婆婆 <tqx5201@163.com>
2026-08-08 19:44:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ async def api_handler(
                     """UPDATE tv_list set content=?, name=?, uptime=? WHERE yys=? AND name=?""",
                     (data, newName, uptime, yys, oldName)
                 )
-                print("执行SQL:update")
+                print(f"执行SQL:update{oldName}----{newName}")
                 msg = "修改数据成功"
             else:
                 sql = """
